5b7311e53cee70e0f2d1936aea16b9a557c6a6f7
alanaamerico22 <145569370+alanaamerico22@users.noreply.github.com>
2024-10-25 08:27:06 -0300

    25.10

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao (
+    'Geografia',
+    'Qual é a capital da fraça?',
+    'Paris'
+)
+
+criaCartao (
+    'Meio ambiente',
+    'Oque é Fotossíntese?',
+    'É o processo pelo qual as plantas utilizam luz solar, água e dióxido de carbono para produzir glicose e oxigênio.'
+)
+
+criaCartao (
+    'Português',
+    'Quem escreveu "Dom Quixote"?',
+    'Miguel de Cervantes'
+)
+
+criaCartao (
+    'Computação',
+    'Oque é algoritmo?',
+    'É uma sequência de passos ou instruções a serem seguidas para resolver um problema ou resolver um problema ou realizar uma tarefa.'
+)
